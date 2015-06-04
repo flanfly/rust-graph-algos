7f124a83f30f3763b48692d5bc3735b8b1c6c108
seu <seu@panopticon.re>
2015-06-04 15:25:10 +0200

    split traits and impl

---
+++ src/traits.rs
@@ -0,0 +1,53 @@
+use std::hash::Hash;
+
+pub trait Graph<'a,V,E> {
+    type Vertex: Clone + Hash + PartialEq + Eq + Ord;
+    type Edge: Clone + Hash + PartialEq + Eq;
+    type Vertices: Iterator<Item=Self::Vertex>;
+    type Edges: Iterator<Item=Self::Edge>;
+    type Incidence: Iterator<Item=Self::Edge>;
+    type Adjacency: Iterator<Item=Self::Vertex>;
+
+    fn edge_label(&self,Self::Edge) -> Option<&E>;
+    fn vertex_label(&self,Self::Vertex) -> Option<&V>;
+    fn source(&self,Self::Edge) -> Self::Vertex;
+    fn target(&self,Self::Edge) -> Self::Vertex;
+}
+
+pub trait IncidenceGraph<'a,V,E>: Graph<'a,V,E> {
+    fn out_degree(&'a self, Self::Vertex) -> usize;
+    fn out_edges(&'a self, Self::Vertex) -> Self::Incidence;
+}
+
+pub trait BidirectionalGraph<'a,V,E>: IncidenceGraph<'a,V,E> {
+    fn in_degree(&'a self, Self::Vertex) -> usize;
+    fn degree(&'a self, Self::Vertex) -> usize;
+    fn in_edges(&'a self, Self::Vertex) -> Self::Incidence;
+}
+
+pub trait AdjacencyGraph<'a,V,E>: Graph<'a,V,E> {
+    fn adjacent_vertices(&'a self, Self::Vertex) -> Self::Adjacency;
+}
+
+pub trait VertexListGraph<'a,V,E>: IncidenceGraph<'a,V,E> + AdjacencyGraph<'a,V,E> {
+    fn vertices(&'a self) -> Self::Vertices;
+    fn num_vertices(&self) -> usize;
+}
+
+pub trait EdgeListGraph<'a,V,E>: Graph<'a,V,E> {
+    fn num_edges(&self) -> usize;
+    fn edges(&'a self) -> Self::Edges;
+}
+
+pub trait AdjacencyMatrixGraph<'a,V,E>: Graph<'a,V,E> {
+    fn edge(&'a self, Self::Vertex, Self::Vertex) -> Option<Self::Edge>;
+}
+
+pub trait MutableGraph<'a,V,E>: Graph<'a,V,E> {
+    fn add_vertex(&mut self,V) -> Self::Vertex;
+    fn add_edge(&mut self,E,Self::Vertex,Self::Vertex) -> Option<Self::Edge>;
+    fn remove_vertex<'t>(&'t mut self,Self::Vertex) -> Option<V>;
+    fn remove_edge(&mut self,Self::Edge) -> Option<E>;
+    fn edge_label_mut(&mut self,Self::Edge) -> Option<&mut E>;
+    fn vertex_label_mut(&mut self,Self::Vertex) -> Option<&mut V>;
+}
