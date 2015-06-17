73b8f60cffdfe0c17e38ce8bf9b6fd5a7c16819a
seu <seu@panopticon.re>
2015-06-17 14:20:27 +0200

    bump version

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+#![crate_name = "graph_algos"]
+#![crate_type = "lib"]
+#![crate_type = "rlib"]
+
 pub mod traits;
 pub mod adjacency_list;
 pub mod adjacency_matrix;
@@ -5,7 +9,7 @@ pub mod adjacency_matrix;
 pub use adjacency_list::AdjacencyList;
 pub use adjacency_matrix::AdjacencyMatrix;
 
-pub use traits::Graph;
+pub use traits::Graph as GraphTrait;
 pub use traits::AdjacencyGraph;
 pub use traits::IncidenceGraph;
 pub use traits::BidirectionalGraph;
