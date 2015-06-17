3896e71827310f6e0dcfab64ec5f5bddbc48bf14
seu <seu@panopticon.re>
2015-06-17 13:50:32 +0200

    reexport impls

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
 pub mod traits;
 pub mod adjacency_list;
 pub mod adjacency_matrix;
+
+pub use adjacency_list::AdjacencyList;
+pub use adjacency_matrix::AdjacencyMatrix;
+
+pub use traits::Graph;
+pub use traits::AdjacencyGraph;
+pub use traits::IncidenceGraph;
+pub use traits::BidirectionalGraph;
+pub use traits::VertexListGraph;
+pub use traits::EdgeListGraph;
+pub use traits::MutableGraph;
+pub use traits::AdjacencyMatrixGraph;
